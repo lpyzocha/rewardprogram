28917f04b709307c08fdec8c853dcbabd281af34
lpyzocha <lukasz.pyzocha@gmail.com>
2023-02-21 17:23:14 +0100

    initial commit

---
+++ src/main/java/pl/lpyzocha/rewardprogram/RewardprogramApplication.java
@@ -0,0 +1,13 @@
+package pl.lpyzocha.rewardprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardprogramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardprogramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lpyzocha/rewardprogram/RewardprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lpyzocha.rewardprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardprogramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
